2b451f9a6547464033c79d550cd0271eb4230cb6
fukuda <youmei.zwei@gmail.com>
2021-03-08 00:40:59 +0900

    テスト作成中

---
+++ streamlit-docker/app.py
@@ -0,0 +1,23 @@
+import os
+import pkg_resources, imp
+
+import streamlit as st
+import spacy_streamlit
+import spacy
+
+# see https://github.com/explosion/spacy-streamlit
+st.title("Spacy Streamlit App")
+
+spacy_model = ["en_core_web_sm" , "ja_core_news_lg", "ja_core_news_md", "ja_core_news_sm"]
+
+# 未ダウンロードのモデルファイルがある場合はダウンロード
+for spacy_model in spacy_models:
+    try:
+        imp.find_module(spacy_model)
+    except ImportError:
+        os.system("python -m spacy download {}".format(spacy_model))
+        imp.reload(pkg_resources)
+
+
+visualizers = ["parser", "ner", "tokens"] # exclude "textcat", "similarity"
+spacy_streamlit.visualize(spacy_model, "Rusty is the best dog in Charlotte, NC.", visualizers)
---
+++ streamlit-docker/readme.txt
@@ -0,0 +1,2 @@
+docker build -t streamlit-docker .
+docker run -dp 8501:8501 streamlit-docker
